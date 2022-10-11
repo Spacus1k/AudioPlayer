73ab88a7c58e95a28ef1b19a6a6dcf755bad666e
Maksim Shpakouski <MShpakouski@ibagroup.eu>
2022-10-11 14:23:29 +0300

    update BottomBarPlayer

--- app/src/main/java/com/example/audioplayer/presentation/ui/components/BottomBarPlayer.kt
+++ app/src/main/java/com/example/audioplayer/presentation/ui/components/BottomBarPlayer.kt
@@ -1,10 +1,13 @@
 package com.example.audioplayer.presentation.ui.components
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Slider
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -40,12 +43,43 @@ fun BottomBarPlayer(
                 modifier = Modifier
             )
         }
-        Slider(
-            valueRange = 0f..100f,
-            value = progress,
-            onValueChange = { onProgressChange(it) }
-        )
+        AudioSlider(progress = progress, onProgressChange = onProgressChange)
     }
+}
+
+@Composable
+fun AudioSlider(
+    progress: Float,
+    onProgressChange: (Float) -> Unit,
+    range: ClosedFloatingPointRange<Float> = 0f..100f
+) {
+    var localSliderValue by remember {
+        mutableStateOf(progress)
+    }
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val isDragged by interactionSource.collectIsDraggedAsState()
+    val isInteracting = isPressed || isDragged
+
+    val sliderValue by derivedStateOf {
+        if (isInteracting) {
+            localSliderValue
+        } else {
+            progress
+        }
+    }
+
+    Slider(
+        valueRange = range,
+        value = sliderValue,
+        onValueChange = {
+            localSliderValue = it
+        },
+        interactionSource = interactionSource,
+        onValueChangeFinished = {
+            onProgressChange(sliderValue)
+        }
+    )
 }
 
 @Composable
@@ -75,8 +109,6 @@ fun AudioInfo(audioFile: AudioFile, modifier: Modifier) {
                 maxLines = 1
             )
         }
-
-
     }
 }
 
